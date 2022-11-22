bf3ee750454fb2b9496e8f0085bd5136e318c67b
Torwent <Torwent@users.noreply.github.com>
2022-11-22 22:01:42 +0100

    fix: minor fixes to osrs-setup.sh

--- osrs-setup.sh
+++ osrs-setup.sh
@@ -13,12 +13,13 @@ sudo cp ~/osrs/tmp/jagexlauncher/jagexlauncher/oldschool/jagexappletviewer.png ~
 sudo chmod 755 ~/osrs/osrs.jar
 rm -rf ~/osrs/tmp
 
-echo 'alias osrs="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation osrs $HOME/osrs/"' | sudo tee --append /etc/bash.bashrc
+echo 'alias osrs="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation jagexappletviewer $HOME/osrs/"' | sudo tee --append ~/.bashrc
+source ~/.bashrc
 
 if [ ! -f /usr/share/applications/osrs.desktop ]; then
 	echo '[Desktop Entry]' | sudo tee --append /usr/share/applications/osrs.desktop
 	echo 'Type=Application' | sudo tee --append /usr/share/applications/osrs.desktop
-	echo 'Exec="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation osrs $HOME/osrs/"' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Exec="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation jagexappletviewer $HOME/osrs/"' | sudo tee --append /usr/share/applications/osrs.desktop
 	echo 'Name=OSRS' | sudo tee --append /usr/share/applications/osrs.desktop
 	echo 'Comment=Official OSRS Client launcher' | sudo tee --append /usr/share/applications/osrs.desktop
 	echo 'Icon=$HOME/osrs/osrs.png' | sudo tee --append /usr/share/applications/osrs.desktop
