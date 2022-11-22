4470be7b07cb4a28f9cef6bb2bac5b9753328fb3
Torwent <dinisgrilo@gmail.com>
2022-11-22 19:24:12 +0100

    feat: osrs and simba installer scripts for linux

---
+++ osrs-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt install default-jre msitools -y
+mkdir ~/osrs
+mkdir ~/osrs/cache
+mkdir ~/osrs/tmp
+
+cd ~/osrs/tmp
+
+wget -O OldSchool.msi http://www.runescape.com/downloads/OldSchool.msi
+msiextract OldSchool.msi
+sudo cp ~/osrs/tmp/jagexlauncher/jagexlauncher/bin/jagexappletviewer.jar ~/osrs/osrs.jar
+sudo cp ~/osrs/tmp/jagexlauncher/jagexlauncher/oldschool/jagexappletviewer.png ~/osrs/osrs.png
+sudo chmod 755 ~/osrs/osrs.jar
+rm -rf ~/osrs/tmp
+
+echo 'alias osrs="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation osrs $HOME/osrs/"' | sudo tee --append /etc/bash.bashrc
+
+if [ ! -f /usr/share/applications/osrs.desktop ] then
+	echo '[Desktop Entry]' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Type=Application' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Exec="java -Djava.class.path=$HOME/osrs/osrs.jar -Dsun.java2d.noddraw=true -Dcom.jagex.config=http://oldschool.runescape.com/jav_config.ws -Duser.home=$HOME/osrs -Dhttps.protocols=TLSv1.2 -Xss2m -Xmx512m -Xms512m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:+UnlockExperimentalVMOptions -XX:+TieredCompilation osrs $HOME/osrs/"' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Name=OSRS' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Comment=Official OSRS Client launcher' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Icon=$HOME/osrs/osrs.png' | sudo tee --append /usr/share/applications/osrs.desktop
+	echo 'Categories=Game' | sudo tee --append /usr/share/applications/osrs.desktop	
+	echo 'Keywords=RuneScape;OSRS;OldSchool;' | sudo tee --append /usr/share/applications/osrs.desktop
+fi
--- setup.sh
+++ setup.sh
@@ -1,1 +1,43 @@
 #!/bin/bash
+sudo apt install libxtst-dev gtk2.0 libgtk2.0-dev libffi-dev libcap2-bin -y
+mkdir ~/Simba
+
+wget -O ~/Simba/Simba.ico https://raw.githubusercontent.com/Villavu/Simba/simba1400/Source/Simba/Simba.ico
+
+case `dpkg --print-architecture` in
+  aarch64)
+    arch="AArch64";;
+  arm64)
+    arch="AArch64";;
+  amd64)
+    arch="Linux64";;
+  x86_64)
+    arch="Linux64";;
+  *)
+	echo "There's no binary for your architecture. You need to manually compile Simba.";
+	Exit 1;;
+exac
+
+echo "Installing Simba-${arch}"  
+wget -O ~/Simba/Simba https://github.com/Villavu/Simba/releases/download/simba1400-release/Simba-${arch}
+
+chmod +x ~/Simba/Simba
+sudo setcap cap_sys_ptrace=eip ~/Simba/Simba
+
+
+if [ "$(type -t simba)" = '' ]; then
+    echo 'alias simba="$HOME/Simba/Simba"' | sudo tee --append /etc/bash.bashrc
+fi
+
+if [ ! -f /usr/share/applications/simba.desktop ] then
+	echo '[Desktop Entry]' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Type=Application' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Name=Simba' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Comment=Simba 1400' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Exec=~/Simba/Simba' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Icon=/home/username/Simba/Simba.ico' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Categories=Game' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'MimeType=text/simba;text/graph;' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Terminal=false' | sudo tee --append /usr/share/applications/simba.desktop
+	echo 'Keywords=Simba;RuneScape;OSRS;' | sudo tee --append /usr/share/applications/simba.desktop
+fi
